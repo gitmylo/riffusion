7cc88e9874fc5f8dbe617dda8549c08e944dba78
Mylo <36931363+gitmylo@users.noreply.github.com>
2023-04-18 09:47:35 +0200

    Auto upgrade pip (and redirect output to `nul`/`/dev/null`)

--- createvenv.bat
+++ createvenv.bat
@@ -1,6 +1,6 @@
 @echo off
 if not exist venv (
     echo Creating venv, please wait...
-    py -m venv venv
+    py -m venv venv --upgrade-deps > nul
     echo Created venv!
 )
--- createvenv.sh
+++ createvenv.sh
@@ -1,9 +1,9 @@
 if ! [ -e "venv" ]; then
   echo Creating venv, please wait...
   if [[ $OSTYPE == "msys" || $OSTYPE == "win32" ]]; then
-    py -m venv venv
+    py -m venv venv --upgrade-deps > /dev/null
   else
-    python3 -m venv venv
+    python3 -m venv venv --upgrade-deps > /dev/null
   fi
   echo Created venv!
 fi
